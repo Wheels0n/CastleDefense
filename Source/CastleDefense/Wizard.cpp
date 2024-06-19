bced3fa93d72c38304e0f6a91351c7b6660512a5
JihoKim <wlgh0655@gmail.com>
2024-06-20 00:09:28 +0900

    캐릭터 생성 키보드/마우스 상화좌우 이동 및 점프 카메라 추가 게임모드 BP 추가후 위저드 클래스로 설정

---
+++ Source/CastleDefense/Wizard.cpp
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Wizard.h"
+
+// Sets default values
+AWizard::AWizard()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	m_pSkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WizardMesh"));
+	check(m_pSkeletalMeshComponent != nullptr);
+	m_pSkeletalMeshComponent->SetupAttachment(RootComponent);
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> wizardAsset(TEXT("/Script/Engine.SkeletalMesh'/Game/BattleWizardPolyart/Meshes/WizardSM.WizardSM'"));
+	if (wizardAsset.Succeeded())
+	{
+		m_pSkeletalMeshComponent->SetSkeletalMesh(wizardAsset.Object);
+		m_pSkeletalMeshComponent->SetRelativeLocation(FVector(0.0f, 0.0f, -95.0f));
+		m_pSkeletalMeshComponent->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
+	}
+
+
+	m_pCamComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
+	check(m_pCamComponent != nullptr);
+	m_pCamComponent->SetupAttachment(RootComponent);
+	m_pCamComponent->SetRelativeLocation(FVector(-250.0f, 0.0f, 0.0f));
+	m_pCamComponent->bUsePawnControlRotation = true;
+}
+
+
+// Called when the game starts or when spawned
+void AWizard::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWizard::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AWizard::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AWizard::StartJump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AWizard::StopJump);
+
+	PlayerInputComponent->BindAxis("Move_Forward", this, &AWizard::MoveForward);
+	PlayerInputComponent->BindAxis("Move_Right", this, &AWizard::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &AWizard::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &AWizard::AddControllerPitchInput);
+}
+
+void AWizard::MoveForward(float value)
+{
+	FVector dir = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
+	AddMovementInput(dir, value);
+}
+
+void AWizard::MoveRight(float value)
+{
+	FVector dir = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
+	AddMovementInput(dir, value);
+}
+
+void AWizard::StartJump()
+{
+	bPressedJump = true;
+}
+
+void AWizard::StopJump()
+{
+	bPressedJump = false;
+}
+
---
+++ Source/CastleDefense/Wizard.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
+#include "Wizard.generated.h"
+
+UCLASS()
+class CASTLEDEFENSE_API AWizard : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AWizard();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+private:
+	UFUNCTION()
+	void MoveForward(float value);
+
+	UFUNCTION()
+	void MoveRight(float value);
+
+	UFUNCTION()
+	void StartJump();
+
+	UFUNCTION()
+	void StopJump();
+private: 
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	USkeletalMeshComponent* m_pSkeletalMeshComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Camera)
+	UCameraComponent* m_pCamComponent;
+};
